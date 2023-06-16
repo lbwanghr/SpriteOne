2c9337c74db69647e6645d56d51222c235b0bdac
Mightypants <lbwanghr@icloud.com>
2023-06-16 17:55:31 +0800

    Initial Commit

---
+++ SpriteOne/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpriteOne
+//
+//  Created by Haoran Wang on 2023/6/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpriteOne/SpriteOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteOneApp.swift
+//  SpriteOne
+//
+//  Created by Haoran Wang on 2023/6/16.
+//
+
+import SwiftUI
+
+@main
+struct SpriteOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
